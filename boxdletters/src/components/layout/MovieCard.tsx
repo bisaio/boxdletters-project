9cdd964e519afa9f2cbfc62d24158b457a5a980c
bisaio <129079375+bisaio@users.noreply.github.com>
2026-02-21 13:01:15 +0000

    FEAT: skeleton nos cards dos filmes

--- boxdletters/src/components/layout/MovieCard.tsx
+++ boxdletters/src/components/layout/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import 'react-tooltip/dist/react-tooltip.css'
 import styles from './styles/MovieCard.module.css'
+import { useState } from 'react';
 
 interface MovieCardProps {
     id: number,
@@ -13,19 +14,26 @@ const moviesIMG = import.meta.env.VITE_IMG
 
 export default function MovieCard({ id, title, release_date, poster_path }: MovieCardProps) {
 
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     const release_year = new Date(release_date).getFullYear();
 
     return (
         <>
             <Link to={`/movie/${id}`} >
                 <div
-                    key={id}
                     className={styles.container}
                     data-tooltip-id="movie-tooltip"
                     data-tooltip-content={`${title} (${release_year})`}
                     data-tooltip-place="top">
                     <div className={styles.card}>
-                        <img src={`${moviesIMG}${poster_path}`} alt={title} />
+                        {!imageLoaded && <div className={styles.skeleton}>{`${title} (${release_year})`}</div>}
+
+                        <img
+                            src={`${moviesIMG}${poster_path}`}
+                            alt={title}
+                            onLoad={() => setImageLoaded(true)}
+                        />
                     </div>
                 </div>
             </Link>
