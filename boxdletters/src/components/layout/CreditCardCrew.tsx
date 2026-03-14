4628e5bd5aa0efae759c10dc2ae74f0a34a29fa2
bisaio <129079375+bisaio@users.noreply.github.com>
2026-03-14 00:04:03 -0300

    WIP: separando crew por department

--- boxdletters/src/components/layout/CreditCardCrew.tsx
+++ boxdletters/src/components/layout/CreditCardCrew.tsx
@@ -1,4 +1,5 @@
 import type MovieCredits from '../../interfaces/MovieCredits'
+import CreditCardCast from './CreditCardCast'
 import styles from './styles/CreditCardCast.module.css'
 
 type CreditCardCrewProps = {
@@ -6,13 +7,32 @@ type CreditCardCrewProps = {
 }
 
 export default function CreditCardCrew({ crew }: CreditCardCrewProps) {
-    const department_list = [...new Set(crew.map(c => c.department))];
-    
+    const crewByDepartment = crew.reduce((acc, member) => {
+        const dept = member.department;
+
+        if (!acc[dept]) {
+            acc[dept] = [];
+        }
+
+        acc[dept].push(member);
+
+        return acc;
+    }, {} as Record<string, MovieCredits["crew"]>)
+
+
     return (
-        <div
-            className={styles.credits}
-        >
-            <p>teste</p>
-        </div>
+        <>
+            {
+                Object.entries(crewByDepartment).map(([department, members]) =>
+                    <div key={department}>
+                        <h3>{department}</h3>
+
+                        {members.map(member =>
+                            <CreditCardCast name={member.name} />
+                        )}
+                    </div>
+                )
+            }
+        </>
     )
 }
